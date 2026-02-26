0fd7ccc3e293acaa4604cadbf3e3c24069f8414b
ps7757 <ps7757@srmist.edu.in>
2026-02-26 09:18:21 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -6,12 +6,28 @@ class Palindrome {
         String javaVersion = System.getProperty("java.version");
         System.out.println("Java Version: " + javaVersion);
     }
+    void checkPalindrome(String s) {
+        int size = s.length();
+        boolean isPalindrome = true;
+        for (int i = 0; i < size; i++) {
+            if (s.charAt(i) != s.charAt(size - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome) {
+            System.out.println("The string is a palindrome");
+        } else {
+            System.out.println("The string is NOT a palindrome");
+        }
+    }
 }
 
 public class PalindromeCheckerApp{
     public static void main(String[] args) {
         Palindrome p = new Palindrome();
         p.start();
+        p.checkPalindrome("abba");
 
     }
 }
